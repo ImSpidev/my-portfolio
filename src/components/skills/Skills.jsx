96c30d7ac3eaf2fde3f2b78ba640d281c027a5c3
Charly Arreola <charly_cto@hotmail.com>
2023-02-06 17:39:57 -0600

    Other Skills added and Styling in SkillCards

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,8 @@
+import {IconContext} from "react-icons";
 import { Container, Row, Col} from 'react-bootstrap';
 import { SkillCard } from '../cards/SkillCard';
+
+import { SiGit, SiFigma, SiMysql, SiBootstrap, SiPostman, SiReact, SiAdobephotoshop, SiFirebase, SiGooglecloud } from "react-icons/si";
 
 import htmlIcon from '../../assets/img/svg/html-5-icon.svg';
 import cssIcon from '../../assets/img/svg/css-3-icon.svg';
@@ -71,6 +74,22 @@ export const Skills = () => {
                                         )
                                     })
                                 }
+                            </div>
+                            <div className="other-skills-container">
+                                <h3 className="cyberpunk-text">Other Skills</h3>
+                                <div className="social-icons">
+                                    <IconContext.Provider value={{size: 60}}>
+                                        <SiGit className="git-icon skills-icons" title="Git"/>
+                                        <SiFigma className="figma-icon skills-icons" title="Figma"/>
+                                        <SiBootstrap className="bootstrap-icon skills-icons" title="Bootstrap"/>
+                                        <SiAdobephotoshop className="photoshop-icon skills-icons" title="Photoshop"/>
+                                        <SiReact className="react-native-icon skills-icons" title="React Native"/>
+                                        <SiMysql className="mysql-icon skills-icons" title="MySQL"/>
+                                        <SiPostman className="postman-icon skills-icons" title="Postman"/>
+                                        <SiFirebase className="firebase-icon skills-icons" title="Firebase"/>
+                                        <SiGooglecloud className="gcp-icon skills-icons" title="Google Cloud Platform"/>
+                                    </IconContext.Provider>
+                                </div>
                             </div>
                         </div>
                     </Col>
