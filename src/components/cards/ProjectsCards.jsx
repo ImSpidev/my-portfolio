237107322ba7f7aa71e85a6d92dfec9708277d7d
Charly Arreola <charly_cto@hotmail.com>
2023-03-07 02:32:57 -0600

    Added modals to each project, with their image, description and link buttons to demo and repo

--- src/components/cards/ProjectsCards.jsx
+++ src/components/cards/ProjectsCards.jsx
@@ -1,17 +1,31 @@
+import  { useState } from 'react';
+import { MyVerticallyCenteredModal } from '../modals/projectsModal';
+
 import './ProjectsCards.css';
 import '../../assets/css/cyberpunk.css'
 
 
-export const ProjectCards = ({ id, src, title }) => {
+export const ProjectCards = ({ id, src, title, description, link, repo }) => {
+    const [modalShow, setModalShow] = useState(false);
+    
     return(
         <>
-            <div className={`div${id}`}>
-                <span></span>
+            <div className={`div${id}`} onClick={() => setModalShow(true)}>
                 <div className="cyber-tile-small fg-dark bg-cyberPink zoom">
                     <img src={src} />
                     <label>{title}</label>
                 </div>
             </div>
+
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                title={title}
+                src={src}
+                description={description}
+                link={link}
+                repo={repo}
+            />
         </>
     );
 }
---
+++ src/components/modals/projectsModal.jsx
@@ -0,0 +1,36 @@
+import { FaImage, FaGithub} from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+import './projectsModal.css';
+
+export const MyVerticallyCenteredModal = (props) => {
+    return (
+    <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+    >
+        <Modal.Header>
+            <Modal.Title className="no-shadow" id="contained-modal-title-vcenter">
+                {props.title}
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <img src={props.src} />
+            <p>
+                {props.description}
+            </p>
+            <div className="social-media-icons">
+                <a href={props.link} target='_blank' className="link">
+                    <FaImage className="web"/>
+                </a>
+                <a href={props.repo} target='_blank' className="link">
+                    <FaGithub className="github"/>
+                </a>
+            </div>
+        </Modal.Body>
+    </Modal>
+    );
+}
