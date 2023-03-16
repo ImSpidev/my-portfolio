d7feb16965b06e077ad8ef7a2a627a78f37f8d8c
Charly Arreola <charly_cto@hotmail.com>
2023-03-15 19:26:45 -0600

    Responsive styles added and changed a class in the projectsModal that afected social media icons

--- src/components/modals/projectsModal.jsx
+++ src/components/modals/projectsModal.jsx
@@ -22,10 +22,10 @@ export const MyVerticallyCenteredModal = (props) => {
                 {props.description}
             </p>
             <div className="social-media-icons">
-                <a href={props.link} target='_blank' className="link">
+                <a href={props.link} target='_blank' className="link-modal">
                     <FaImage className="web"/>
                 </a>
-                <a href={props.repo} target='_blank' className="link">
+                <a href={props.repo} target='_blank' className="link-modal">
                     <FaGithub className="github"/>
                 </a>
             </div>
